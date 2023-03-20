f017cb2480374ff190e203be76a289cb30f8b93b
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-03-20 19:17:06 +0500

    Booking

--- resources/js/Pages/Booking/Index.vue
+++ resources/js/Pages/Booking/Index.vue
@@ -91,34 +91,10 @@ defineProps({
                                             :href="
                                                 route('booking.pdf', booking.id)
                                             "
-                                            class="btn btn-primary btn-sm m-1"
+                                            class="focus:outline-none text-white bg-purple-500 hover:bg-purple-600 focus:ring-2 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-purple-900"
                                             target="_blank"
-                                            >Print Invoice</a
+                                            >Invoice</a
                                         >
-                                        <!-- <Link
-                                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                                            :href="
-                                                route(
-                                                    'booking.edit',
-                                                    booking.id
-                                                )
-                                            "
-                                            >Detail</Link
-                                        >
-
-                                        <button
-                                            type="button"
-                                            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                                        >
-                                            Delete
-                                        </button>
-
-                                        <button
-                                            type="button"
-                                            class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
-                                        >
-                                            Invoice
-                                        </button> -->
                                     </td>
                                 </tr>
                             </tbody>
