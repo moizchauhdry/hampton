28fec813d87fcb4657db4245b41a9a2910ef8f3e
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-04-10 02:20:49 +0500

    #email_payment_button

--- resources/views/mails/booking.blade.php
+++ resources/views/mails/booking.blade.php
@@ -4,9 +4,28 @@
     .table table,
     .table th,
     .table td {
-        border: 1px solid rgb(194, 189, 189);
+        border: 1px solid rgb(194, 189, 189) !important;
         padding: 5px;
         font-size: 14px;
+    }
+
+    .paynow-button {
+        background-color: #0070C0;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        font-size: 16px;
+        padding: 10px 20px;
+        cursor: pointer;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .paynow-button:hover {
+        background-color: #005EA6;
+    }
+
+    a {
+        text-decoration: none;
     }
 </style>
 @section('content')
@@ -16,7 +35,7 @@
 </div>
 
 <div style="margin-top:20px; margin-bottom:20px">
-    <a
+    <a class="paynow-button"
         href="https://hamptonchauffer.com/payment/checkout.php?pickup={{$booking->pickup}}&&destination={{$booking->destination}}&&amount={{$booking->price}}">Paynow</a>
 </div>
 
