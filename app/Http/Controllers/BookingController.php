975db2be7c0f10914294a6b6dbb0e86ad1afc449
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-06-12 00:27:04 +0500

    #send_grid_email

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -89,7 +89,7 @@ class BookingController extends Controller
         try {
 
             $booking_date = date('m/d/Y h:i A', strtotime($booking->booking_date));
-            $payment_url = 'https://app.hamptonchauffer.com/payment/checkout.php?pickup=' . $booking->pickup . "&&destination=" . $booking->destination . ' Booking Date: ' . $booking_date . '&&amount=' . $booking->price;
+            $payment_url = 'https://app.hamptonchauffer.com/payment/checkout.php?pickup=' . $booking->pickup . "&&destination=" . $booking->destination . ' Booking Date: ' . $booking_date . '&&amount=' . $booking->total_price;
 
             $data = [
                 "personalizations" => [
